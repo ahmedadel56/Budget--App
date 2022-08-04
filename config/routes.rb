825af4a99ffdbc9843f78736b12d2e494faa4537
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-04 12:30:15 +0200

    Add routes and home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "splash_screen#index"
+  resources :categories do
+    resources :entities
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
