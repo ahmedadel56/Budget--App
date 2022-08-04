9b0d16b754d580395032262195650dd3e49e4449
ahmedadel56 <ahmdadl563@gmail.com>
2022-08-05 00:27:03 +0200

    Add some tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true
+  validates :name, presence: true, length: { minimum: 3 }
   validates :email, :password, presence: true, length: { minimum: 3 }
   has_many :entities
   has_many :categories
